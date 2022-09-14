eb84ad9964d3e5649390b9bd9625fe931c5ae780
Jonathan Zapatero <j.zapatero@audinate.com>
2022-09-14 15:20:04 +1000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3_bucket" {
-  source = "app.terraform.io/TF_training_140922/s3-bucket"
+  source = "github.com/jz-ad8/terraform-aws-s3-bucket"
 
   bucket = "my-s3-bucket"
   acl    = "private"
